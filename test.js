a92472ef3a8f38abfc6a7c6fded891bd27b3aa70
mattpilla <kaztalek@gmail.com>
2017-03-24 21:09:18 -0400

    initial commit

---
+++ test.js
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/test.html');
+});
+
+io.on('connection', socket => {
+    console.log('user connected');
+    socket.on('chat message', msg => {
+        io.emit('chat message', msg);
+    });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+http.listen(3000, () => {
+    console.log('lets do this shit');
+});
